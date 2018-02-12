7e855e61166e1169be732683ad5cc85292cb45cc
Romain Allée <romain.allee@epitech.eu>
2018-02-12 23:35:40 +0100

    init commit

---
+++ desktop/src/com/rcorp/airpokecross/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rcorp.airpokecross.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rcorp.airpokecross.AirPokeCrossGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AirPokeCrossGame(), config);
+	}
+}
